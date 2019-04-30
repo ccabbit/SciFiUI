c648e6fafc2dd9bba978edaaf9dd4780313e3c56
Seungmin Lee <Seungmin Lee>
2019-04-30 19:47:46 +0100

    Breif write of Comet

--- java/src/ie/tudublin/Comet.java
+++ java/src/ie/tudublin/Comet.java
@@ -1,3 +1,26 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+
+public class Comet
+{
+    private float x;
+    private float y;
+    private float r;
+    private float g;
+    private float b;
+    private float diameter;
+    private float radius;
+    UI ui;
+
+    public Comet (UI ui,float x, float y, float diameter, float r, float g, floatb)
+    {
+        this.ui = ui;
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.diameter = diameter;
+    }
+}
